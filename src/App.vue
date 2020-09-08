2f7e068b884394d03c582efab40a7df2788f035a
yuanzhian <yuanzhian@cai-inc.com>
2020-09-08 14:52:22 +0800

    feat:test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    
-    <HelloWorld msg="Welcome to Your docker-test App"/>
+
+    <HelloWorld msg="Welcome to Your docker-test"/>
   </div>
 </template>
 
